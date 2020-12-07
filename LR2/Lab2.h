28e0e8dab8b7f03e03c367dc53578e7ac3e4c1db
gtcoda <gtcoda@gmail.com>
2020-12-07 22:08:50 +0300

    Fix: LR2

---
+++ LR2/Lab2.h
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+using namespace std;
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+class Lab2 {
+	static string input;
+	static string output;
+
+public:
+	static void exec();
+
+};
