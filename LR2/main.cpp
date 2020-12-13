e63954a506ccc0ba0bc86a24fa92006672921c49
gtcoda <gtcoda@gmail.com>
2020-12-13 21:08:05 +0300

    Добавлены рисунки.

--- LR2/main.cpp
+++ LR2/main.cpp
@@ -14,6 +14,51 @@ using namespace std;
 #include "Lab2.h"
 
 int main() {
-		Lab2 m = new Lab2();
-		m.exec();
+	//	Lab2 m = new Lab2();
+	//	m.exec();
+
+	std::string input;
+	std::stringstream output;
+	std::vector<string> tok;
+
+	cout << "Enter text:" << endl;
+	std::getline(cin, input);
+
+	char * pch = strtok((char *)input.c_str(), " ,.-"); // во втором параметре указаны разделитель (пробел, запятая, точка, тире)
+
+	while (pch != NULL) {
+		string str = pch;
+		tok.push_back(pch);
+		std::cout << pch << "\n\r";
+		pch = strtok(NULL, " ,.-");
+
+	}
+
+	for (vector<string>::iterator it = tok.begin(); it < tok.end(); it++) {
+		(*it).c_str();
+
+		if (isdigit((*it).c_str()[0])) {
+			int m = atoi((*it).c_str());
+			char stri[6] = "";
+			_itoa_s(m, stri, 6, 16);
+
+			for (int r = 0; (r < 6) && (stri[r]); r++) {
+				if (stri[r] >= 'a' && stri[r] <= 'z') stri[r] -= 'z' - 'Z';
+			}
+
+			string str = stri;
+			str = "0x" + str;
+			(*it) = str;
+		}
+	}
+
+	string s;
+
+	for (vector<string>::iterator it = tok.begin(); it < tok.end(); it++) {
+		s = s + " " + (*it);
+	}
+
+	s.erase(0, 1); // удалим первый пробел
+	cout << s;
+
 }
